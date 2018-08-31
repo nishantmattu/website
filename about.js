1357809062564af5f2f94eff66500495c11630ed
Nishant Mattu <nishantmattu@Nishants-MacBook-Pro.local>
2018-08-30 21:38:49 -0700

    update website look

--- about.js
+++
@@ -1,7 +0,0 @@
-$("html").ready(function() {
-
-	$("#nishantImgTwo").fadeIn(1000, function() {
- 		console.log("fade done callback");	
- 	});
-	console.log("fade done");
-});
--- index.js
+++
@@ -1,6 +0,0 @@
-
-$("html").ready(function() {
-
-	$(".homeImages .thumbnail").animate( { opacity:1 }, 1000 );;
-	console.log("fade done");
-});
